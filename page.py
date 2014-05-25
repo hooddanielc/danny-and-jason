552403a061aa1c44060ea2cf16543843b4d8fbe2
hooddanielc <hood.danielc@gmail.com>
2014-05-25 00:25:57 -0700

    Add images and fix viewport meta tag

--- page.py
+++ page.py
@@ -93,6 +93,7 @@ class Page(BodyBuilder):
     <head>
       <meta charset="utf-8">
       <title>$title</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       $head_css
       $head_js
     </head>
